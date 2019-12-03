9b16596a3b7b40d22d3d5427972269798ed01aba
liyu <yyu_li@kingdee.com>
2019-12-03 10:26:11 +0800

    push test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,14 +18,14 @@ public class Main {
 
     private static void start() {
         // 创建机器人对象 ( 传入配置 )
-        PicqBotX bot = new PicqBotX(new PicqConfig(31031)
+        PicqBotX bot = new PicqBotX(new PicqConfig(31033)
                 .setDebug(true)
 //                .setSecret("This is secret")
 //                .setAccessToken("Brq4KSm+3UdaUJnLZ+AJfj**v-vePWL$")
         );
 
         // 添加一个机器人账户 ( 名字, 发送URL, 发送端口 )
-        bot.addAccount("RabbitBot", "127.0.0.1", 31030);
+        bot.addAccount("RabbitBot", "127.0.0.1", 31032);
 //        bot.addAccount("Bot01", "127.0.0.1", 31091);
 
         // 启用HyExp ( 非必要 )
